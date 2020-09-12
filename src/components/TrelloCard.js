bae017ed8aea5c5df28a8eafe8a7e3614de8f77f
BonDah18 <proctorine@gmail.com>
2020-09-11 23:06:06 -0700

    more corrections

--- src/components/TrelloCard.js
+++ src/components/TrelloCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
-import {Draggalbe} from "react-beautiful-dnd";
+import {Draggable} from "react-beautiful-dnd";
 import styled from "styled-components";
 
 const CardContainer = styled.div`
@@ -11,7 +11,7 @@ const CardContainer = styled.div`
 
 function TrelloCard({text, id, index}) {
   return(
-    <Draggable draggableid={String(id)} index={index}>
+    <Draggable draggableId={String(id)} index={index}>
     {provided => (
       <CardContainer
       ref={provided.innerRef} 
